403bf1fa6d3d0b90dbc761feab6522fb0dcb706d
Lars Willemsens <sami.filjak@student.kdg.be>
2023-12-26 21:28:01 +0100

    nieuw

--- HiFresh_Groep02/src/test/java/HiFresh/stepDefs/BereidingsstapToevoegen.java
+++ HiFresh_Groep02/src/test/java/HiFresh/stepDefs/BereidingsstapToevoegen.java
@@ -6,26 +6,43 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class BereidingsstapToevoegen {
+    private ReceptService receptService;
+    private ReceptRepository receptRepository;
 
-
+public BereidingsstapToevoegen() {
+        receptRepository = new ReceptRepository();
+        receptService = new ReceptService(receptRepository);
+    }
 
     @When("ik een bereidingsstap {string} toevoeg aan recept met id {int}")
     public void ikEenBereidingsstapToevoegAanReceptMetId(String arg0, int arg1) {
+        Bereidingsstap bereidingsstap = new Bereidingsstap(arg0);
+        receptService.addBereidingsstap(bereidingsstap, arg1);
     }
 
     @Then("heeft het recept {int} bereidingsstappen")
     public void heeftHetReceptBereidingsstappen(int arg0) {
+        Recept recept = receptService.getReceptById(arg0);
+        assertEquals(recept.getBereidingsstappen().size(), 1);
     }
 
     @And("de laatste bereidingstap voor recept {int} heeft beschrijving {string}")
     public void deLaatsteBereidingstapVoorReceptHeeftBeschrijving(int arg0, String arg1) {
+        Recept recept = receptService.getReceptById(arg0);
+        Bereidingsstap bereidingsstap = recept.getBereidingsstappen().get(0);
+        assertEquals(bereidingsstap.getBeschrijving(), arg1);
     }
 
     @When("ik een bereidingsstap {string} toevoeg aan recept met id {int} na stap {int}")
     public void ikEenBereidingsstapToevoegAanReceptMetIdNaStap(String arg0, int arg1, int arg2) {
+        Bereidingsstap bereidingsstap = new Bereidingsstap(arg0);
+        receptService.addBereidingsstap(bereidingsstap, arg1, arg2);
     }
 
     @And("de bereidingsstap {int} voor recept {int} heeft beschrijving {string}")
     public void deBereidingsstapVoorReceptHeeftBeschrijving(int arg0, int arg1, String arg2) {
+        Recept recept = receptService.getReceptById(arg1);
+        Bereidingsstap bereidingsstap = recept.getBereidingsstappen().get(arg0);
+        assertEquals(bereidingsstap.getBeschrijving(), arg2);
     }
 }
--- HiFresh_Groep02/src/test/java/HiFresh/stepDefs/GemiddeldeAankoopprijsRaadplegen.java
+++ HiFresh_Groep02/src/test/java/HiFresh/stepDefs/GemiddeldeAankoopprijsRaadplegen.java
@@ -5,19 +5,28 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class GemiddeldeAankoopprijsRaadplegen {
-    @Given("het is vandaag {int}{int}{int}")
-    public void hetIsVandaag(int arg0, int arg1, int arg2) {
+    private ReceptService receptService;
+    private LocalDate vandaag;
+
+    public GemiddeldeAankoopprijsRaadplegen(ReceptService receptService) {
+        this.receptService = receptService;
+    }
+
+    @Given("het is vandaag {string}")
+    public void hetIsVandaag(String datum) {
+        this.vandaag = LocalDate.parse(datum);
     }
 
     @When("ik de gemiddelde aankoopprijs van het recept {int} raadpleeg")
     public void ikDeGemiddeldeAankoopprijsVanHetReceptRaadpleeg(int arg0) {
+        receptService.getGemiddeldeAankoopprijs(arg0, vandaag);
     }
 
     @Then("krijg ik dat de gemiddelde aankoopprijs van het recept {int} gelijk is aan {double}")
     public void krijgIkDatDeGemiddeldeAankoopprijsVanHetReceptGelijkIsAan(int arg0, int arg1, int arg2) {
+        double berekendePrijs = receptService.getGemiddeldeAankoopprijs(arg0, vandaag);
+        assertEquals(berekendePrijs, arg2);
     }
 
-    @Then("krijg ik dat de gemiddelde aankoopprijs van recept {int} gelijk is aan {double}")
-    public void krijgIkDatDeGemiddeldeAankoopprijsVanReceptGelijkIsAan(int arg0, int arg1, int arg2) {
-    }
+
 }
